a8c931652f604edff5bc016a879e8a46076ec48c
jaehyun <vljh246v@naver.com>
2020-04-27 22:18:28 +0900

    event 학습

---
+++ day3/event/src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import EventPractice from "./EventPractice";
+import EventPracticeFunction from "./EventPracticeFunction";
+
+const App = () => {
+  return <EventPracticeFunction />;
+};
+
+export default App;
